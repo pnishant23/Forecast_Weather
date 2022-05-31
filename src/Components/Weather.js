da0cfc9e6e693ca9a2af36fd289b117b36af63eb
pnishant23 <pnishant2323@gmail.com>
2022-05-31 16:40:31 +0530

    1 file modified

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -7,17 +7,23 @@ import CurrentDetails from './CurrentDetails';
 export default function Weather({ weather }) {
   return (
     <>
-      <div className="flex flex-col justify-center items-center">
-        <h1>Mumbai,IND</h1>
-        <div className="flex items-center">
-          <div>
-            30<sup>o</sup>C
+      <div className="bg-gray-200 rounded-md mx-3 p-2 grid">
+
+        <div className="flex flex-col justify-center items-center">
+          <h1>Mumbai,IND</h1>
+          <div className="flex items-center">
+            <div>
+              30<sup>o</sup>C
+            </div>
+            <div>icon</div>
           </div>
-          <div>icon</div>
         </div>
+
+        <Hourly />
+
+        <CurrentDetails />
+
       </div>
-      <Hourly />
-      <CurrentDetails />
     </>
   );
 }
